5df6b2d1fdcb952c38b5c5154992c84dd5f9b273
ApChris <achris.info@gmail.com>
2020-03-15 19:01:34 +0200

    fix date function

--- src/date.c
+++ src/date.c
@@ -7,47 +7,89 @@
 #include "../include/date.h"
 
 
-long Compare_Date_Time(Date * date,Date * dateArg)
+long Compare_Date(Date * date,Date * dateArg)
 {
-    if(dateArg -> year > date -> year) // if last transaction year is > than new transaction year then ERROR
+    // return 0 -> date == dateArg
+    // return 1 -> date > dateArg
+    // return -1 -> date < dateArg
+    if(dateArg -> year > date -> year)
     {
-        printf("\n1Wrong date/time input!!!\n");
         return -1;
     }
     else if(dateArg -> year < date -> year)
     {
-        printf("\nThe date & time from stdin is fine!\n");
-        return 0;
+        return 1;
     }
     else // dateArg -> year is == YEAR
     {
         if(dateArg -> month > date -> month)
         {
-            printf("\n2Wrong date/time input!!!\n");
             return -1;
         }
         else if(dateArg -> month < date -> month)
         {
-            printf("\nThe date & time from stdin is fine!\n");
-            return 0;
+            return 1;
         }
         else // year & month is the same
         {
             if(dateArg -> day > date -> day)
             {
-                printf("\n3Wrong date/time input!!!\n");
                 return -1;
             }
             else if(dateArg -> day < date -> day)
             {
-                printf("\nThe date & time from stdin is fine!\n");
-                return 0;
+                return 1;
             }
             else // year/month/day is the same
             {
-                printf("\nThe date & time is exactly the same!!\n");
+                return 0;
             }
         }
     }
     return 0;
 }
+//
+// long Compare_Date(Date * date,Date * dateArg)
+// {
+//     if(dateArg -> year > date -> year)
+//     {
+//         printf("\n1Wrong date/time input!!!\n");
+//         return -1;
+//     }
+//     else if(dateArg -> year < date -> year)
+//     {
+//         printf("\nThe date & time from stdin is fine!\n");
+//         return 0;
+//     }
+//     else // dateArg -> year is == YEAR
+//     {
+//         if(dateArg -> month > date -> month)
+//         {
+//             printf("\n2Wrong date/time input!!!\n");
+//             return -1;
+//         }
+//         else if(dateArg -> month < date -> month)
+//         {
+//             printf("\nThe date & time from stdin is fine!\n");
+//             return 0;
+//         }
+//         else // year & month is the same
+//         {
+//             if(dateArg -> day > date -> day)
+//             {
+//                 printf("\n3Wrong date/time input!!!\n");
+//                 return -1;
+//             }
+//             else if(dateArg -> day < date -> day)
+//             {
+//                 printf("\nThe date & time from stdin is fine!\n");
+//                 return 0;
+//             }
+//             else // year/month/day is the same
+//             {
+//                 printf("\nThe date & time is exactly the same!!\n");
+//             }
+//         }
+//     }
+//     return 0;
+// }
