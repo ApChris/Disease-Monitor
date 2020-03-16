2afc575c3b018cc1d206492a03cb5c961cbeae5e
ApChris <achris.info@gmail.com>
2020-03-16 16:30:02 +0200

    Update bst struct with patientinfo pointer

--- include/bst.h
+++ include/bst.h
@@ -2,10 +2,11 @@
 #define __BST_H_
 
 #include "date.h"
-
+#include "hash.h"
 typedef struct Node
 {
     Date * entryDate;
+    PatientInfo * info;
     struct Node * left;
     struct Node * right;
 }Node;
@@ -15,11 +16,11 @@ typedef struct BST
     Node * root;
 }BST;
 
-Node * CreateNode(Date * entryDate);
+Node * CreateNode(Date * entryDate, PatientInfo * info);
 BST * CreateBST();
-Node * PushBST(Node * node, Date * entryDate);
+Node * PushBST(Node * node, Date * entryDate, PatientInfo * info);
 void inorder(Node * node);
 Node * DeleteNode(Node * node, Date * entryDate);
-long SearchBST(BST * root,Date * entryDate);
+long SearchBST(BST * root,Date * entryDate, PatientInfo * info);
 void DeallocateBST(BST * temp);
 #endif
