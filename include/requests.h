61fa2c3ff4682bbccfa88e5671985b7ba312e473
ApChris <achris.info@gmail.com>
2020-03-18 14:02:46 +0200

    initial commit

---
+++ include/requests.h
@@ -0,0 +1,9 @@
+#ifndef _REQUESTS_H_
+#define _REQUESTS_H_
+#include <stdio.h>
+
+#include "hashDC.h"
+
+void Read_Requests(Hash_DC * diseaseHash, Hash_DC * countryHash, Hash * patientHash);
+
+#endif
