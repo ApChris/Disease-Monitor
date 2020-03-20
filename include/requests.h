770a8f09782246731cdc227d14e59a4b14191e71
ApChris <achris.info@gmail.com>
2020-03-20 17:55:19 +0200

    add declarations of request5&7

--- include/requests.h
+++ include/requests.h
@@ -3,8 +3,9 @@
 #include <stdio.h>
 
 #include "hashDC.h"
-
+bool Request_5(Hash * patientHash, Hash_DC * diseaseHash, Hash_DC * countryHash, char * tok);
 bool Request_6(Hash * patientHash, char * tok);
+bool Request_7(Hash_DC * diseaseHash, char * tok);
 void Read_Requests(Hash_DC * diseaseHash, Hash_DC * countryHash, Hash * patientHash);
 
 #endif
