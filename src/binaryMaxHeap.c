248b5bdd821195d713f73fb0b79f0804f6177faf
ApChris <achris.info@gmail.com>
2020-03-24 21:09:44 +0200

    update binarymaxheap

--- src/binaryMaxHeap.c
+++ src/binaryMaxHeap.c
@@ -87,17 +87,10 @@ void setParent(BinaryTreeNode * node,BinaryTreeNode * parent)
 
 BinaryTreeNode * BuildMaxHeap(BinaryTreeNode ** arr, BinaryTreeNode * node, size_t i, size_t n)
 {
-    // BinaryMaxHeap * maxHeap = CreateBinaryMaxHeap();
+
 
     if(i < n)
     {
-        // if(node == NULL)
-        // {
-        //     BinaryTreeNode * newBinaryTreeNode = CreateBinaryTreeNode(arr[i] -> dc_name, arr[i] -> total_patientes);
-        //     node = newBinaryTreeNode;
-        //     return node;
-        // }
-
 
         BinaryTreeNode * newBinaryTreeNode =  CreateBinaryTreeNode(arr[i] -> dc_name, arr[i] -> total_patientes);
         node = newBinaryTreeNode;
@@ -120,44 +113,6 @@ BinaryTreeNode * BuildMaxHeap(BinaryTreeNode ** arr, BinaryTreeNode * node, size
     return node;
 }
 
-
-BinaryTreeNode * BuildMaxHeap2(BinaryTreeNode ** heap, BinaryTreeNode * node, size_t i, size_t n)
-{
-    if(i < n)
-    {
-
-        BinaryTreeNode * newBinaryTreeNode =  CreateBinaryTreeNode(head -> dc_name, arr[i] -> total_patientes);
-        node = newBinaryTreeNode;
-
-        printf("%ld\n", head -> total_patientes);
-        node -> left = BuildMaxHeap2(heap, node -> left, 2*i+1 ,n);;
-
-        printf("12->%ld\n", head -> total_patientes);
-
-        node -> right = BuildMaxHeap2(heap, node -> right, 2*i+2 ,n);
-
-        // right -> parent = node;
-        printf("r->%ld\n", arr[i] -> total_patientes);
-        return node;
-
-    }
-
-    return node;
-}
-void swap2(BinaryTreeNode * node1, BinaryTreeNode * node2)
-{
-    BinaryTreeNode * temp = node1 -> left;
-    node1 -> left = node2 -> left;
-    node2 -> left = temp;
-
-    temp = node1 -> right;
-    node1 -> right = node2-> right;
-    node2 -> right = temp;
-
-    temp = node1 -> parent;
-    node1 -> parent = node2-> parent;
-    node2 -> parent = temp;
-}
 void MaxHeapify(BinaryTreeNode * root, BinaryTreeNode * node)
 {
     if(root == NULL)
@@ -184,142 +139,17 @@ void preorderMaxHeapify(BinaryTreeNode * root)
     preorderMaxHeapify(root -> right);
 }
 
-// BinaryTreeNode *right = PushMaxHeap(node -> right, dc_name, total_patientes);
-//             node -> right = right;
-//             right -> parent = node;
+void GetKMaxValues(BinaryTreeNode * root, size_t k)
+{
+    for (size_t i = 0; i < k; i++)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        printf("-> %s -> %ld\n",root -> dc_name, root -> total_patientes);
+        root -> total_patientes = 0;
+        preorderMaxHeapify(root);
+    }
 
-//
-// void getCurrentPatients(BinaryTreeNode * node)
-// {
-//     if(node != NULL)
-//     {
-//         getCurrentPatients(node -> left);
-//         if(node -> info -> exitDate -> day == TAG)
-//         {
-//             tResult++;
-//             PatientInfo_Print(node -> info);
-//         }
-//         getCurrentPatients(node -> right);
-//     }
-// }
-//
-// void getPatientsInThatPeriod(BinaryTreeNode * node, Date * entryDate, Date * exitDate)
-// {
-//     if(node != NULL)
-//     {
-//         getPatientsInThatPeriod(node -> left, entryDate, exitDate);
-//         if( (Compare_Date(node -> info -> entryDate, entryDate) != -1) && (Compare_Date(node -> info -> exitDate, exitDate) != 1) )
-//         {
-//             tResult++;
-//             PatientInfo_Print(node -> info);
-//         }
-//         getPatientsInThatPeriod(node -> right, entryDate, exitDate);
-//     }
-// }
-//
-// // A function which finds the patients from specific country in spesific period
-// void getPatientsInThatPeriod_SpecifiCountry(BinaryTreeNode * node, Date * entryDate, Date * exitDate, char * country)
-// {
-//     if(node != NULL)
-//     {
-//         getPatientsInThatPeriod_SpecifiCountry(node -> left, entryDate, exitDate, country);
-//         if(!strcmp(node -> info -> country, country))
-//         {
-//             // check date
-//             if( (Compare_Date(node -> info -> entryDate, entryDate) != -1) && (Compare_Date(node -> info -> exitDate, exitDate) != 1) )
-//             {
-//                 tResult++;
-//                 PatientInfo_Print(node -> info);
-//             }
-//         }
-//
-//         getPatientsInThatPeriod_SpecifiCountry(node -> right, entryDate, exitDate, country);
-//     }
-// }
-//
-// BinaryTreeNode * PushMaxHeap(BinaryTreeNode * node, char * dc_name, size_t total_patientes)
-// {
-//     if(node == NULL)
-//     {
-//         BinaryTreeNode * newBinaryTreeNode = CreateBinaryTreeNode(dc_name, total_patientes);
-//         node = newBinaryTreeNode;
-//         return node;
-//     }
-//
-//     if(total_patientes <= node -> total_patientes)
-//     {
-//         if(node -> right == NULL && node -> left != NULL)
-//         {
-//             BinaryTreeNode *right = PushMaxHeap(node -> right, dc_name, total_patientes);
-//             node -> right = right;
-//             right -> parent = node;
-//
-//             if(right -> parent -> total_patientes < node -> right -> total_patientes)
-//             {
-//                 printf("mphka edw1\n");
-//                 swap(&(right -> parent -> dc_name), &(node -> right -> dc_name));
-//                 swap(&(right -> parent -> total_patientes), &(node -> right -> total_patientes));
-//
-//                 // printf("Pasdasd = %s , %ld\n",right -> parent -> dc_name, right -> parent -> total_patientes);
-//                 // printf("123123 = %s , %ld\n",node -> right -> dc_name, node -> right -> total_patientes);
-//             }
-//         }
-//         else
-//         {
-//             BinaryTreeNode *left = PushMaxHeap(node -> left, dc_name, total_patientes);
-//             node -> left = left;
-//             left -> parent = node;
-//
-//             if(left -> parent -> total_patientes < node -> left -> total_patientes)
-//             {
-//                 printf("mphka edw2\n");
-//
-//                 swap(&(left -> parent -> dc_name), &(node -> left -> dc_name));
-//                 swap(&(left -> parent -> total_patientes), &(node -> left -> total_patientes));
-//             }
-//         }
-//
-//     }
-//     else
-//     {
-//         if(node -> left == NULL && node -> right != NULL)
-//         {
-//             BinaryTreeNode *left = PushMaxHeap(node -> left, dc_name, total_patientes);
-//             node -> left = left;
-//             left -> parent = node;
-//
-//             if(left -> parent -> total_patientes < node -> left -> total_patientes)
-//             {
-//                 printf("mphka edw3\n");
-//
-//                 swap(&(left -> parent -> dc_name), &(node -> left -> dc_name));
-//                 swap(&(left -> parent -> total_patientes), &(node -> left -> total_patientes));
-//             }
-//         }
-//         else
-//         {
-//             BinaryTreeNode *right = PushMaxHeap(node -> right, dc_name, total_patientes);
-//             node -> right = right;
-//             right -> parent = node;
-//
-//
-//                 if(right -> parent -> total_patientes < node -> right -> total_patientes)
-//                 {
-//                     printf("mphka edw4\n");
-//                     swap(&(right -> parent -> dc_name), &(node -> right -> dc_name));
-//                     swap(&(right -> parent -> total_patientes), &(node -> right -> total_patientes));
-//
-//                     // printf("Pasdasd = %s , %ld\n",right -> parent -> dc_name, right -> parent -> total_patientes);
-//                     // printf("123123 = %s , %ld\n",node -> right -> dc_name, node -> right -> total_patientes);
-//                 }
-//         }
-//
-//
-//
-//     }
-//     return node;
-// }
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
+}
